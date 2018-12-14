a59537032bcd86291d4862e0ffa9ef42109537c5
Michel <michel.naslund@gmail.com>
2018-12-14 12:17:27 +0100

    Add load_data function to train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,8 +1,43 @@
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+import nltk
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import precision_recall_fscore_support as score
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
 import sys
+import pandas as pd
+import numpy as np
+import pickle
+
+import warnings
+warnings.filterwarnings("ignore")
 
 
 def load_data(database_filepath):
-    pass
+    '''
+    Loads table and returns X, Y variables as well as the category names.
+
+    INPUT
+        database_filepath (string): The path to the database.
+
+    OUTPUT:
+        X (ndarray): The message data - features
+        Y (ndarray): The categories - targets
+        categories (list): Labels for the categories
+    '''
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('DisasterMessages', con=engine)
+    X = df['message'].values
+    Y = df.iloc[:, 4:].values
+    categories = list(df.columns[4:])
+
+    return X, Y, categories
 
 
 def tokenize(text):
@@ -26,14 +61,15 @@ def main():
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+        X_train, X_test, Y_train, Y_test = train_test_split(
+            X, Y, test_size=0.2)
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
@@ -43,9 +79,9 @@ def main():
         print('Trained model saved!')
 
     else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
+        print('Please provide the filepath of the disaster messages database '
+              'as the first argument and the filepath of the pickle file to '
+              'save the model to as the second argument. \n\nExample: python '
               'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
 
 
